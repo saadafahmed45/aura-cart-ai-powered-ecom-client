acdd1867e0e91848d03f52fc7def777b9c663c6d
saadafahmed45 <hafijnahid45@gmail.com>
2026-07-02 04:41:44 +0600

    added new sever api link

--- src/lib/api.js
+++ src/lib/api.js
@@ -9,7 +9,7 @@ const api = axios.create({
   }
 });
 
-api.interceptors.request.use(
+api.interceptors.request.use( 
   (config) => {
     const token = useAuthStore.getState().accessToken;
     if (token) {
