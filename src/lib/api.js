e79acc404e1ede56b052cd2e55db01162b6baf0a
saadafahmed45 <hafijnahid45@gmail.com>
2026-07-03 02:06:55 +0600

    added api fixed

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: "https://aura-cart-server.vercel.app/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
