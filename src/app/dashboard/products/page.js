9b44f73eb35e55901dfd4cfd7416d1caff4648a0
saadafahmed45 <hafijnahid45@gmail.com>
2026-07-03 03:21:11 +0600

    set the slider swip

--- src/app/dashboard/products/page.js
+++ src/app/dashboard/products/page.js
@@ -178,7 +178,7 @@ export default function ProductsManagement() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start">
         
         <div className="lg:col-span-2 space-y-4">
           {isLoading ? (
